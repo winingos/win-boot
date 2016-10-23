bc6a95c8d79164c0b5295641f2811303ef897d0f
wining <wangningos@sina.com>
2016-10-23 22:05:10 +0800

    Optional 用法

--- src/test/java/com/comment/java8/OptionalDemo.java
+++ src/test/java/com/comment/java8/OptionalDemo.java
@@ -19,6 +19,15 @@ public class OptionalDemo {
 
         System.out.println(fullName.map(s -> "hey "+s+"!").orElse(")-("));
         fullName.ifPresent(s -> System.out.println("s = " + s));
+
+        fullName.ifPresent(System.out::println);
+        //要使用新的视角来看待Optional 对null的处理
+        System.out.println(fullName.map(s->s.toUpperCase()).orElse("---"));
+
+        System.out.println(fullName.map(s->s.getBytes()).map(String::new).orElse("Nothing"));
+
+
+
     }
     @Test
     public  void test(){
