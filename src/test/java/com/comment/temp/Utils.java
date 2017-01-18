0c472ac094089c89572505fc50bb3b76eb2d65af
ning.wang <ning.wang@mtime.com>
2017-01-18 11:19:17 +0800

    md5

---
+++ src/test/java/com/comment/temp/Utils.java
@@ -0,0 +1,33 @@
+package com.comment.temp;
+
+import java.security.MessageDigest;
+
+/**
+ * Created by 王宁 on 2017/1/18.
+ */
+public class Utils {
+    //阿里md5 摘要算法
+    public static String md5(String str) {
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.reset();
+            messageDigest.update(str.getBytes("UTF-8"));
+        } catch (Exception e) {
+//            logger.warn("计算MD5摘要失败", e);
+            return null;
+        }
+
+        byte[] byteArray = messageDigest.digest();
+        StringBuffer md5StrBuff = new StringBuffer();
+        for (int i = 0; i < byteArray.length; i++) {
+            if (Integer.toHexString(0xFF & byteArray[i]).length() == 1)
+                md5StrBuff.append("0").append(Integer.toHexString(0xFF & byteArray[i]));
+            else
+                md5StrBuff.append(Integer.toHexString(0xFF & byteArray[i]));
+        }
+
+        return md5StrBuff.toString();
+    }
+
+}
