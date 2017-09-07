249edd66d3582258964d655c4403b94c918f2ee0
ning.wang <ning.wang@mtime.com>
2017-09-07 15:34:50 +0800

    InterruptibleSocketTest

--- src/test/java/com/javacore/InterruptibleSocketTest.java
+++ src/test/java/com/javacore/InterruptibleSocketTest.java
@@ -3,12 +3,27 @@ package com.javacore;
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.channels.SocketChannel;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Administrator on 17/7/29.
  */
 public class InterruptibleSocketTest {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+                    JFrame frame = new InterruptibleSocketFrame();
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    frame.setVisible(true);
+                }
+        );
+    }
 }
 
 class InterruptibleSocketFrame extends JFrame {
@@ -57,31 +72,122 @@ class InterruptibleSocketFrame extends JFrame {
             connectTrd.start();
         });
 
-        cancelBtn=new JButton("Cancel");
+        cancelBtn = new JButton("Cancel");
         cancelBtn.setEnabled(false);
         northPanel.add(cancelBtn);
         cancelBtn.addActionListener(e -> {
             connectTrd.interrupt();
             cancelBtn.setEnabled(false);
         });
-        server=new TestServer();
+        server = new TestServer();
         new Thread(server).start();
     }
 
+    /**
+     * Connects to the test server, using blocking I/O
+     *
+     * @throws IOException
+     */
 
-    private void connectBlocking()throws IOException {
-
+    private void connectBlocking() throws IOException {
+        messages.append("Blocking: \n");
+        Socket sock = new Socket("localhost", 8189);
+        try {
+            Scanner in = new Scanner(sock.getInputStream());
+            processer(in);
+        } finally {
+            sock.close();
+            EventQueue.invokeLater(() -> {
+                messages.append("Socket closed\n");
+                interruptibleBtn.setEnabled(true);
+                blockingBtn.setEnabled(true);
+            });
+        }
     }
 
+    private void processer(Scanner in) {
+        while (!Thread.currentThread().isInterrupted()) {
+            messages.append("Reading ");
+            if (in.hasNextLine()) {
+                String s = in.nextLine();
+                messages.append(s);
+                messages.append("\n");
+            }
+        }
+    }
+
+    /**
+     * Connects to the test server, using interruptible I/O
+     *
+     * @throws IOException
+     */
 
     private void connectInterruptibly() throws IOException {
+        messages.append("Interruptible:\n");
+        SocketChannel channel = SocketChannel.open(new InetSocketAddress("localhost", 8189));
 
+        try {
+            in = new Scanner(channel);
+            processer(in);
+        } finally {
+            channel.close();
+            EventQueue.invokeLater(() -> {
+                messages.append("Channel closed\n");
+                interruptibleBtn.setEnabled(true);
+                blockingBtn.setEnabled(true);
+            });
+        }
     }
 
-    class TestServer implements Runnable{
+    /**
+     * A multi-threaded server that listens to port 8189 and send numbers to the client,
+     * simulating a hanging server after 10 numbers.
+     */
+    class TestServer implements Runnable {
 
         @Override
         public void run() {
+            try (ServerSocket s = new ServerSocket(8189)) {
+                while (true) {
+                    Socket incoming = s.accept();
+                    Runnable r = new TestServerHandler(incoming);
+                    new Thread(r).start();
+                }
+            } catch (IOException e) {
+                messages.append("\nTestServer.run: " + e);
+            }
+        }
+    }
+
+    /**
+     * This class handles the client input for one server socket connection
+     */
+    class TestServerHandler implements Runnable {
+        private Socket incoming;
+        private int counter;
+
+        /**
+         * Constructs a handler
+         */
+        public TestServerHandler(Socket i) {
+            this.incoming = i;
+        }
+
+        @Override
+        public void run() {
+            try (OutputStream outStream = incoming.getOutputStream()) {
+
+                PrintWriter out = new PrintWriter(outStream, true);
+                while (counter < 100) {
+                    counter++;
+                    if (counter <= 10) out.println(counter);
+                    TimeUnit.MILLISECONDS.sleep(1000);
+                }
+                incoming.close();
+                messages.append("Closing server\n");
+            } catch (Exception e) {
+                messages.append("\nTestServerHandler.run: " + e);
+            }
 
         }
     }
