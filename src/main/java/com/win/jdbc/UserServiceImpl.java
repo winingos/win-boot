008188de74bf91c01c4e8a0c19cc75d2523b2b96
wining <wangningos@sina.com>
2018-04-27 08:00:28 +0800

    update pom

--- src/main/java/com/win/jdbc/UserServiceImpl.java
+++ src/main/java/com/win/jdbc/UserServiceImpl.java
@@ -19,13 +19,13 @@ public class UserServiceImpl implements UserService {
     @Override
     public void save(User user) {
         jdbcTemplate.update("insert into user(name,age,sex)values(?,?,?)",
-                new Object[]{user.getName(), user.getAge(), user.getSex(),
-                        new int[]{Types.VARCHAR, Types.INTEGER, Types.VARCHAR}
-                });
+                new Object[]{user.getName(), user.getAge(), user.getSex()},
+                new int[]{Types.VARCHAR, Types.INTEGER, Types.VARCHAR}
+        );
     }
 
     @Override
     public List<User> getUsers() {
-        return jdbcTemplate.query("select * from user",new UserMapper());
+        return jdbcTemplate.query("select * from user", new UserMapper());
     }
 }
